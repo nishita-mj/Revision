dc2697368c1ffdc22a47657074b037dba0a786f7
nishita-mj <manjrawalanishita@gmail.com>
2025-09-26 21:08:18 +0530

    add leap year file program

---
+++ Day-2/leap-year.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int year;
+
+    cout << "Enter the Year : ";
+    cin >> year ;
+
+    if ((year % 4 == 0 && year % 100 != 0) || year % 400 ==0)
+    {
+        cout << "This is a leap year...";
+    }else{
+        cout << "This is not a leap year...";
+    }
+
+    return 0;
+}
